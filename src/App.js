2a2dff15cb7501074ff37b9a11e6d106147038af
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-05-31 23:57:44 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  //Addition function
+  const sum = () => {
+    const answer = Number(input) + Number(result);
+    setResult(answer);
+  };
+
+  //Subtraction function
+  const subtraction = () => {
+    const answer = Number(result) - Number(input);
+    setResult(answer);
+  };
+
+  //Multiplication function
+  const multiplication = () => {
+    const answer = Number(result) * Number(input);
+    setResult(answer);
+  };
+
+  //Division function
+  const divison = () => {
+    const answer = Number(result) / Number(input);
+    setResult(answer);
+  };
+
+  const handleReset = () => {
+    setInput("");
+  };
+
+  const handleResetResult = () => {
+    setResult("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="App">
+        <p className="result">Result : {result}</p>
+        <input
+          type="number"
+          className="input"
+          placeholder="type number"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <div className="btn-container">
+          <button className="btn" onClick={sum}>
+            add
+          </button>
+          <button className="btn" onClick={subtraction}>
+            subtract
+          </button>
+          <button className="btn" onClick={multiplication}>
+            multiply
+          </button>
+          <button className="btn" onClick={divison}>
+            divide
+          </button>
+
+          <button className="btn reset-btn" onClick={handleReset}>
+            reset input
+          </button>
+          <button className="btn reset-btn" onClick={handleResetResult}>
+            reset result
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
